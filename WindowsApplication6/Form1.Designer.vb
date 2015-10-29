f81c130e4973ec1618aba6130c9c4818538a01ce
racingairforce <a8332436000@yahoo.com.tw>
2015-10-30 00:29:35 +0800

    111111111

---
+++ WindowsApplication6/Form1.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.l1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtusername = New System.Windows.Forms.TextBox()
+        Me.txtpassword = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("新細明體", 20.0!)
+        Me.Button1.Location = New System.Drawing.Point(83, 342)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(144, 87)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "登入"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("新細明體", 20.0!)
+        Me.Button2.Location = New System.Drawing.Point(348, 342)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(145, 86)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "結束"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'l1
+        '
+        Me.l1.AutoSize = True
+        Me.l1.Font = New System.Drawing.Font("新細明體", 15.0!)
+        Me.l1.Location = New System.Drawing.Point(111, 56)
+        Me.l1.Name = "l1"
+        Me.l1.Size = New System.Drawing.Size(49, 20)
+        Me.l1.TabIndex = 2
+        Me.l1.Text = "帳號"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("新細明體", 15.0!)
+        Me.Label2.Location = New System.Drawing.Point(113, 175)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "密碼"
+        '
+        'txtusername
+        '
+        Me.txtusername.Location = New System.Drawing.Point(310, 56)
+        Me.txtusername.Name = "txtusername"
+        Me.txtusername.Size = New System.Drawing.Size(151, 22)
+        Me.txtusername.TabIndex = 4
+        '
+        'txtpassword
+        '
+        Me.txtpassword.Location = New System.Drawing.Point(310, 175)
+        Me.txtpassword.Name = "txtpassword"
+        Me.txtpassword.Size = New System.Drawing.Size(151, 22)
+        Me.txtpassword.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(591, 502)
+        Me.Controls.Add(Me.txtpassword)
+        Me.Controls.Add(Me.txtusername)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.l1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents l1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtusername As System.Windows.Forms.TextBox
+    Friend WithEvents txtpassword As System.Windows.Forms.TextBox
+
+End Class
---
+++ WindowsApplication6/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+
+  
+    Private Sub txtusername_TextChanged(sender As Object, e As EventArgs) Handles txtusername.TextChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+
+        If txtusername.Text = "login" Then
+        End If
+        If txtpassword.Text = "abc123" Then
+            MsgBox("帳戶密碼正確")
+        Else
+            txtusername.BackColor = Color.MistyRose
+
+            txtpassword.BackColor = Color.MistyRose
+
+
+        End If
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Application.Exit()
+    End Sub
+End Class
